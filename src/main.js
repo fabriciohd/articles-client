4fe26149283feddf640ec0e740197c97a56fd02c
Fabricio Cunha <fabricio_hd@hotmail.com>
2021-05-25 13:43:39 -0300

    configurando dependencias

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import store from './config/store'
+import router from './config/router'
+import vuetify from './plugins/vuetify'
+import toasted from 'vue-toasted';
+import vue2Editor from "vue2-editor";
+
+Vue.config.productionTip = false;
+
+Vue.use(toasted);
+Vue.use(vue2Editor);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
